da1bbfeaf5c95e0ad630f8edf9a087d9ed8ecad2
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-20 20:04:08 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,7 +25,10 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.chomp
-  if 
+  songs.each do |song 
+  if choice.include?(*1..songs.length+1) || choice.include? 
     
   else
-    puts 
+    puts "Invalid input, please try again"
+  end
+end  
